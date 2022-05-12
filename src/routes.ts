1a7bda2422e472e6a9968ca60003c7838c7a99e8
Laize Ferraz <laizeferraz@gmail.com>
2022-05-12 12:11:46 +1000

    add try catch to find the error

--- src/routes.ts
+++ src/routes.ts
@@ -8,18 +8,26 @@ export const routes = express.Router()
 
 routes.post('/feedbacks', async (req, res) => {
     const {type, comment, screenshot} = req.body
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodemailerMailAdapter
-    )
 
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot
-    })
+    try {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+        const nodemailerMailAdapter = new NodemailerMailAdapter()
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+        )
 
-    return res.status(201).send()
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot
+        })
+
+        return res.status(201).send()
+    }catch (err) {
+        console.log(err)
+
+        return res.status(400).send()
+    }
+    
 })
